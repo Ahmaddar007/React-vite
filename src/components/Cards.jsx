6ef86ae3eb673f0defed92136f4e8261d84c9d7e
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-09-22 01:38:25 +0500

    ...

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -19,7 +19,7 @@ function Cards() {
         <div className="flex justify-center items-center relative w-1/2 h-full bg-[#192826] rounded-xl">
           <img
             className="w-32"
-            src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"
+            src="https://ochi.design/wp-content/uploads/2022/04/logo002.svg"
             alt=""
           />
           <button className="absolute left-10 bottom-10 px-3 py-1 border-2 rounded-full">
