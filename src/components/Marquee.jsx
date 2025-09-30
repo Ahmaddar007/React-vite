d6c7f895f6da237201be01e79a079fff66e01fd5
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-09-30 23:29:02 +0500

    ....

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -28,7 +28,7 @@ function Marquee() {
             text-white/80
           "
         >
-          • Design • Innovation • Excellence • Creativity • • Imagination •
+          • Design • Innovation • Excellence • Creativity • Imagination •
         </motion.h1>
       </div>
     </div>
